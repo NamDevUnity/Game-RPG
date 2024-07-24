3530c58518d91413750c36cd3fbb55c233ad2b6e
Duong Dinh Nam <duongdinhnam000@gmail.com>
2024-07-24 20:54:49 +0700

    kho do
    kho do

--- Assets/Scripts/UI/UI_ItemSlot.cs
+++ Assets/Scripts/UI/UI_ItemSlot.cs
@@ -9,8 +9,14 @@ public class UI_ItemSlot : MonoBehaviour
 
     public InventoryItem item;
 
-    void Start()
+   
+
+    public void UpdateSlot(InventoryItem _newItem)
     {
+        item = _newItem;
+
+        itemImage.color = Color.white;
+
         if (item != null)
         {
             itemImage.sprite = item.data.icon;
